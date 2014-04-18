d0a5b830ca0b382c62cbb9c5909eb2398d34011a
Rykugur <rollhax@gmail.com>
2014-04-17 22:11:44 -0500

    Updating un/install scripts

--- install.sh
+++ install.sh
@@ -7,12 +7,14 @@ cd $HOME_DIR
 
 # TODO: create a text file externally that the install and uninstall scripts will read from,
 # instead of hard-coding these files here.                               
+# TODO: don't create file if it already exists?
 ln -s $DOTFILES_DIR/i3 .i3
 ln -s $DOTFILES_DIR/vim .vim
 ln -s $DOTFILES_DIR/vimrc .vimrc
+ln -s $DOTFILES_DIR/prezto .zprezto
 ln -s $DOTFILES_DIR/xinitrc .xinitrc
 ln -s $DOTFILES_DIR/Xresources .Xresources
-ln -s $DOTFILES_DIR/prezto .zprezto
+ln -s $DOTFILES_DIR/pentadactylrc .pentadactylrc
 ln -s $DOTFILES_DIR/prezto/runcoms/zlogin .zlogin
 ln -s $DOTFILES_DIR/prezto/runcoms/zlogout .zlogout
 ln -s $DOTFILES_DIR/prezto/runcoms/zpreztorc .zpreztorc
--- uninstall.sh
+++ uninstall.sh
@@ -6,4 +6,4 @@ cd $HOME_DIR
 
 # TODO: create a text file externally that the install and uninstall scripts will read from,
 # instead of hard-coding these files here.
-rm -f .i3 .vim .vimrc .xinitrc .Xresources .zprezto .zlogin .zlogout .zpreztorc .zprofile .zshenv .zshrc
+rm -f .i3 .vim .vimrc .zprezto .xinitrc .Xresources .pentadactylrc .zlogin .zlogout .zpreztorc .zprofile .zshenv .zshrc
