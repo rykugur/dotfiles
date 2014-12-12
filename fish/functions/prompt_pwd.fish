9189c7dd6337ad56c0eb02915ff70446227fd7a1
Rykugur <rollhax@gmail.com>
2014-12-12 07:30:17 -0600

    Moving two more personal functions

---
+++ fish/functions/prompt_pwd.fish
@@ -0,0 +1,10 @@
+#
+
+function prompt_pwd --description 'Print the current working directory, NOT shortened to fit the prompt'
+  if test "$PWD" != "$HOME"
+      printf "%s" (echo $PWD|sed -e "s|^$HOME|~|")
+      #echo $PWD|sed -e 's|/private||' -e "s|^$HOME|~|"
+  else
+      echo '~'
+  end
+end
