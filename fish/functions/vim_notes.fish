3d35f6f38d431928b1c228cfa2bdf8f6fb2dc33b
Rykugur <rollhax@gmail.com>
2015-01-07 07:51:48 -0600

    Renamed local variable to be a bit more generic, updating scripts

--- fish/functions/vim_notes.fish
+++ fish/functions/vim_notes.fish
@@ -1,22 +1,22 @@
 #
 
 function vim_notes --description "opens the given file name in vim notes plugin, uses dmenu as input; usage: vim_notes"
-  # ensure we have an owncloud notes variable defined
-  if test -z $OWNCLOUD_NOTES
+  # ensure we have a notes directory variable defined
+  if test -z $NOTES_DIR
     return
   end
 
   # ensure notes dir exists, else return right away
-  if test ! -e $OWNCLOUD_NOTES
+  if test ! -e $NOTES_DIR
     return
   end
 
   # cd to this dir to make searching eas(y|ier)
-  cd $OWNCLOUD_NOTES
+  cd $NOTES_DIR
 
   # pass an empty string to dmenu to trick it, but also so it won't return anything if nothing is entered
   # use basename to get the file name only
-  set -l note_file (find -L $OWNCLOUD_NOTES -type f | grep -v '^\.$' | sed 's!\.\/!!' | /usr/bin/dmenu -l 10)
+  set -l note_file (find -L $NOTES_DIR -type f | grep -v '^\.$' | sed 's!\.\/!!' | /usr/bin/dmenu -l 10)
 
   if test -z $note_file
     # escape was likely pressed, run away
