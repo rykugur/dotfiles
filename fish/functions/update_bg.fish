7cb6ff0c525d06c987d450e8c471797a4f5a27fa
rykugur <rollhax@gmail.com>
2015-10-20 07:34:39 -0500

    Updated update_bg script to be less tarded

--- fish/functions/update_bg.fish
+++ fish/functions/update_bg.fish
@@ -1,7 +1,9 @@
 #
 
 function update_bg --description "Updates the background using feh; assumes feh is available to the user AND the DESKTOP_BG variable exists and points to an image file that feh can understand"
-  if test -n $DESKTOP_BG
+  if test -z $DESKTOP_BG
     feh --bg-fill ~/.dotfiles/backgrounds/mieke_at_emmas.jpg
+  else 
+    feh --bg-fill $DESKTOP_BG
   end
 end
