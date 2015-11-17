9b8c88e9058c4ad328a22c868d6351c874f1d257
rykugur <rollhax@gmail.com>
2015-11-17 10:18:40 -0600

    ofmg, I'm going home

--- configs/luakit/globals.lua
+++ configs/luakit/globals.lua
@@ -68,7 +68,7 @@ search_engines = {
     cp           = "http://balescream:32113",
     trans        = "http://balescream:32116",
     -- cheapo bookmarks
-    keto         = "http://www.redit.com/r/keto",
+    keto         = "http://www.reddit.com/r/keto",
     sub          = "http://www.reddit.com/r/%s",
     trp          = "http://www.reddit.com/me/m/trp",
 }
