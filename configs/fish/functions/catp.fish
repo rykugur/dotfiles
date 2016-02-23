10e1494b31ab327dbc1cf1cadcd70434cb242355
rykugur <rollhax@gmail.com>
2016-02-22 21:52:02 -0600

    Adding catp function

---
+++ configs/fish/functions/catp.fish
@@ -0,0 +1,18 @@
+#
+
+function catp --description 'cats the given file to the primary selection buffer'
+  set -l argc (count $argv)
+  set -l _usage "cats the contents of the given file to the primary selection buffer. Usage: catp [file]"
+
+  if test $argc -lt 1
+    echo $_usage
+    return 1
+  end
+
+  if test ! -f $argv
+    echo "Specified path wasn't a file; path=$argv"
+    return 1
+  end
+
+  cat $argv | xclip -i
+end
