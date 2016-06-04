47bb474185d5d8110e81743183a37822793406bb
rykugur <rollhax@gmail.com>
2016-06-03 20:25:19 -0500

    Revert "Update config.fish."
    This reverts commit 08af453c8f6518d87e8b8339e86867ac377759aa.

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -1,10 +1,32 @@
-# Path to Oh My Fish install.
-set -gx OMF_PATH "/Users/dusty/.local/share/omf"
+# this file exists because I use the same setup at work, but don't want work-specific config files
+# on my public github account. It's sourced by local config.fish files, along with other fish
+# config files.
 
-# Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG "/Users/dusty/.config/omf"
+# source fisherman
+set fisher_home ~/.dotfiles/deps/fisherman
+set fisher_config ~/.config/fisherman
+set fisher_file ~/.dotfiles/configs/fish/fisherfile
+source $fisher_home/fisher.fish
 
-# Load oh-my-fish configuration.
-source $OMF_PATH/init.fish
+set fish_function_path $fish_function_path $HOME/.dotfiles/configs/fish/functions
 
-source $HOME/.dotfiles/configs/fish/config.fish
+set PATH $PATH $HOME/bin
+set PATH $PATH $GOBIN
+
+# source our exports file
+source $HOME/.dotfiles/configs/fish/exports.fish
+
+# source our aliases file
+source $HOME/.dotfiles/configs/fish/aliases.fish
+
+# source our abbreviations file
+source $HOME/.dotfiles/configs/fish/abbreviations.fish
+
+# don't greet me!
+set fish_greeting ""
+
+# Load any local configs
+# Do this last, since we might want to append to/override abbreviations, aliases, etc.
+if test -e $HOME/.fish_local.fish
+  source $HOME/.fish_local.fish
+end
