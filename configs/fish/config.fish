b0ae650f5af07be2c387ab7be44aea30d740ac05
rykugur <rollhax@gmail.com>
2016-06-03 19:40:36 -0500

    Added fisher and updated fish config to work with it and new oh-my-fish.

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -2,42 +2,25 @@
 # on my public github account. It's sourced by local config.fish files, along with other fish
 # config files.
 
-# Path to your oh-my-fish.
-set fish_path $HOME/.oh-my-fish
-
-# Theme
-set fish_theme rollhax
-
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
-# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-# Example format: set fish_plugins autojump bundler
-set fish_plugins sublime
-
-# Load oh-my-fish configuration.
-. $fish_path/oh-my-fish.fish
-
 # source fisherman
 set fisher_home ~/.dotfiles/deps/fisherman
 set fisher_config ~/.config/fisherman
 set fisher_file ~/.dotfiles/configs/fish/fisherfile
-source $fisher_home/config.fish
+source $fisher_home/fisher.fish
 
-# Other potential fish function directories
-if test -e $HOME/.dotfiles/configs/fish/functions
-  _append_path $HOME/.dotfiles/configs/fish/functions fish_function_path
-end
+set fish_function_path $fish_function_path $HOME/.dotfiles/configs/fish/functions
 
-_append_path $HOME/bin PATH
-_append_path $GOBIN PATH
+set PATH $PATH $HOME/bin
+set PATH $PATH $GOBIN
 
 # source our exports file
-. $HOME/.dotfiles/configs/fish/exports.fish
+source $HOME/.dotfiles/configs/fish/exports.fish
 
 # source our aliases file
-. $HOME/.dotfiles/configs/fish/aliases.fish
+source $HOME/.dotfiles/configs/fish/aliases.fish
 
 # source our abbreviations file
-. $HOME/.dotfiles/configs/fish/abbreviations.fish
+source $HOME/.dotfiles/configs/fish/abbreviations.fish
 
 # don't greet me!
 set fish_greeting ""
@@ -45,5 +28,5 @@ set fish_greeting ""
 # Load any local configs
 # Do this last, since we might want to append to/override abbreviations, aliases, etc.
 if test -e $HOME/.fish_local.fish
-  . $HOME/.fish_local.fish
+  source $HOME/.fish_local.fish
 end
