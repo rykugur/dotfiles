08af453c8f6518d87e8b8339e86867ac377759aa
rykugur <rollhax@gmail.com>
2016-06-03 20:24:26 -0500

    Update config.fish.

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -1,32 +1,10 @@
-# this file exists because I use the same setup at work, but don't want work-specific config files
-# on my public github account. It's sourced by local config.fish files, along with other fish
-# config files.
+# Path to Oh My Fish install.
+set -gx OMF_PATH "/Users/dusty/.local/share/omf"
 
-# source fisherman
-set fisher_home ~/.dotfiles/deps/fisherman
-set fisher_config ~/.config/fisherman
-set fisher_file ~/.dotfiles/configs/fish/fisherfile
-source $fisher_home/fisher.fish
+# Customize Oh My Fish configuration path.
+#set -gx OMF_CONFIG "/Users/dusty/.config/omf"
 
-set fish_function_path $fish_function_path $HOME/.dotfiles/configs/fish/functions
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
 
-set PATH $PATH $HOME/bin
-set PATH $PATH $GOBIN
-
-# source our exports file
-source $HOME/.dotfiles/configs/fish/exports.fish
-
-# source our aliases file
-source $HOME/.dotfiles/configs/fish/aliases.fish
-
-# source our abbreviations file
-source $HOME/.dotfiles/configs/fish/abbreviations.fish
-
-# don't greet me!
-set fish_greeting ""
-
-# Load any local configs
-# Do this last, since we might want to append to/override abbreviations, aliases, etc.
-if test -e $HOME/.fish_local.fish
-  source $HOME/.fish_local.fish
-end
+source $HOME/.dotfiles/configs/fish/config.fish
