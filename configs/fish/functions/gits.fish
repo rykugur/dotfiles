50e130fbaebafa726f228ca85b0282dc03f570d1
rykugur <rollhax@gmail.com>
2016-07-16 17:46:02 -0500

    Reticulating splines...

--- configs/fish/functions/gits.fish
+++ configs/fish/functions/gits.fish
@@ -5,18 +5,16 @@ function gits --description "cd wrapper for gits dir"
   set -l _base_dir "$HOME/gits"
   set -l _usage "Usage: gits [dir]"
 
-  if test $argc -lt 1
-    echo $_usage
-  end
-
   # set a sane default
   set -l _dir "dotfiles"
-  switch $argv[1]
-  case sync or s
-    set _dir "sync"
-  case dotfiles or df or dots or d
-    set _dir "dotfiles"
-  end # end switch
+  if test $argc -gt 0
+    switch $argv[1]
+    case sync or s
+      set _dir "sync"
+    case dotfiles or df or dots or d
+      set _dir "dotfiles"
+    end # end switch
+  end # end if
 
   cd $_base_dir/$_dir
 end
