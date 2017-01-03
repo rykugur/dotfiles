c2de0f14767483aec3674df04248edb51941eec9
Rykugur <rollhax@gmail.com>
2017-01-03 13:51:39 -0600

    Converted gits to use getopts

--- configs/fish/functions/gits.fish
+++ configs/fish/functions/gits.fish
@@ -10,17 +10,19 @@ function gits --description "cd wrapper for gits dir"
   end
 
   set -l _base_dir $GITS
-
   set -l _dir ""
-  if test $argc -gt 0
-    switch $argv[1]
-    case sync or s
-      set _dir "sync"
-    case dotfiles or df or dots or d
-      set _dir "dotfiles"
-    case fish or f
-      set _dir "dotfiles/configs/fish"
-    end # end switch
+
+  if test $argc -ge 1
+    getopts $argv | while read -l key value
+      switch $key
+        case sync or s
+          set _dir "sync"
+        case dotfiles or df or dots or d
+          set _dir "dotfiles"
+        case fish or f
+          set _dir "dotfiles/configs/fish"
+      end # end switch
+    end # end getopts
   end # end if
 
   cd $_base_dir/$_dir
