916ec6441db9dff2e61046576f88a6919c35ec3d
Rykugur <rollhax@gmail.com>
2017-02-14 10:09:23 -0600

    Moving a bunch of stuff to exports.fish

--- configs/fish/exports.fish
+++ configs/fish/exports.fish
@@ -3,3 +3,37 @@ set -gx GITS "$HOME/gits"
 set -gx VISUAL "vim"
 set -gx GOPATH "$HOME/code/go"
 set -gx GOBIN "$GOPATH/bin"
+
+# set additional paths
+set PATH $PATH $HOME/bin
+
+# golang!
+if not set -q GOPATH
+  set -gx GOPATH "$HOME/code/go"
+end
+if not set -q GOBIN
+  set -gx GOBIN "$GOPATH/bin"
+end
+set PATH $PATH $GOBIN
+
+# set GITS
+if not set -q GITS
+  set -gx GITS "$HOME/gits"
+end
+
+# set paste/copy aliases
+if getos --mac
+  set -gx PASTECMD 'pbpaste'
+  set -gx COPYCMD  'pbcopy'
+else
+  set -gx PASTECMD 'xclip -o'
+  set -gx COPYCMD  'xclip -i'
+end
+
+# set DROPBOX_DIR
+if not set -q DROPBOX_DIR
+  set -gx DROPBOX_DIR "$HOME/dropbox/Dropbox"
+end
+
+# don't greet me!
+set fish_greeting ""
--- configs/fish/omf/init.fish
+++ configs/fish/omf/init.fish
@@ -6,35 +6,6 @@ source $fisher_home/fisher.fish
 
 set -gx fish_function_path $fish_function_path $HOME/.dotfiles/configs/fish/functions
 
-# set additional PATHs
-set PATH $PATH $HOME/bin
-if not set -q GOPATH
-  set -gx GOPATH "$HOME/code/go"
-end
-if not set -q GOBIN
-  set -gx GOBIN "$GOPATH/bin"
-end
-set PATH $PATH $GOBIN
-
-# set GITS
-if not set -q GITS
-  set -gx GITS "$HOME/gits"
-end
-
-# set DROPBOX_DIR
-if not set -q DROPBOX_DIR
-  set -gx DROPBOX_DIR "$HOME/dropbox/Dropbox"
-end
-
-# set PASTECMD/COPYCMD based on OS
-if getos --mac
-  set -gx PASTECMD 'pbpaste'
-  set -gx COPYCMD  'pbcopy'
-else
-  set -gx PASTECMD 'xclip -o'
-  set -gx COPYCMD  'xclip -i'
-end
-
 # source our exports file
 source $HOME/.dotfiles/configs/fish/exports.fish
 
@@ -43,9 +14,6 @@ source $HOME/.dotfiles/configs/fish/aliases.fish
 
 # source our abbreviations file
 source $HOME/.dotfiles/configs/fish/abbreviations.fish
-
-# don't greet me!
-set fish_greeting ""
 
 # Load any local configs
 # Do this last, since we might want to append to/override abbreviations, aliases, etc.
