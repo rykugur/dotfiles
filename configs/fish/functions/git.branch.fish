524b7c656ca21cf61361dbf7c28de50b05fa56e7
rykugur <rollhax@gmail.com>
2019-04-04 12:40:02 -0500

    fuck it

--- configs/fish/functions/git.branch.fish
+++ configs/fish/functions/git.branch.fish
@@ -1,7 +1,3 @@
 function git.branch --description "Spits out the branch name of the current git repo, or returns status 1 (error) if we're not in a git branch"
-  if git.is_branch
-    echo (git status 2> /dev/null | head -n1 | awk "{print \$3}")
-  else
-    return 1
-  end
+  echo (git status 2> /dev/null | head -n1 | awk "{print \$3}")
 end
--- configs/fish/functions/git.is_dirty.fish
+++
@@ -1,9 +0,0 @@
-function git.is_dirty --description "Determines whether we're in a git repo or not, and if we are wether we're dirty. Return 1 if dirty, 0 otherwise"
-  if git.is_branch
-    if test -z (git status --short)
-      return 0
-    else
-      return 1
-    end
-  end
-end
