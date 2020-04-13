3fde3851fa7260c330eeda4f8504bcdf4cbc9cc7
rykugur <rollhax@gmail.com>
2020-04-12 22:55:06 -0500

    renamed abbrs

--- configs/fish/abbr.tmux.fish
+++ configs/fish/abbr.tmux.fish
@@ -1,6 +1,6 @@
 ### tmux abbreviations
 
-abbr --add --global tmux.at tmux a -t
-abbr --add --global tmux.new tmux new
-abbr --add --global tmux.news tmux new -s
-abbr --add --global tmux.ls tmux ls
+abbr --add --global tmat tmux a -t
+abbr --add --global tmnew tmux new
+abbr --add --global tms tmux new -s
+abbr --add --global tmls tmux ls
