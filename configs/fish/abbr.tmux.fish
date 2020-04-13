67e5ba4e246be00a22aedfcf9009235e8a723e79
rykugur <rollhax@gmail.com>
2020-04-12 22:52:59 -0500

    add tmux abbrs

---
+++ configs/fish/abbr.tmux.fish
@@ -0,0 +1,6 @@
+### tmux abbreviations
+
+abbr --add --global tmux.at tmux a -t
+abbr --add --global tmux.new tmux new
+abbr --add --global tmux.news tmux new -s
+abbr --add --global tmux.ls tmux ls
