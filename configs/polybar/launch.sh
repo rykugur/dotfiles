ef937ea275e35118e469ac99e76af4397af2874a
rykugur <rollhax@gmail.com>
2023-03-24 12:34:22 -0500

    lots of changes and customizations to eos' community i3

--- configs/polybar/launch.sh
+++ configs/polybar/launch.sh
@@ -9,6 +9,6 @@ polybar-msg cmd quit
 # Launch bars
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar primary 2>&1 | tee -a /tmp/polybar1.log & disown
-# polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+polybar secondary 2>&1 | tee -a /tmp/polybar2.log & disown
 
 echo "Bars launched..."
