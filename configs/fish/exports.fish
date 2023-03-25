087895e952521266921506e68b2147f782f8883d
rykugur <rollhax@gmail.com>
2023-03-24 22:48:27 -0500

    get rid of jank ssh_auth_sock setting in dotfiles (gross)

--- configs/fish/exports.fish
+++ configs/fish/exports.fish
@@ -1,31 +1,24 @@
 set -gx CODE_DIR "$HOME/code"
 set -gx GITS "$HOME/gits"
-set -gx VISUAL "vim"
-
-switch (uname)
-  case Darwin
-    set -gx SSH_AUTH_SOCK ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
-  case Linux
-    set -gx SSH_AUTH_SOCK ~/.1password/agent.sock
-end
+set -gx VISUAL vim
 
 ### set additional paths
 fish_add_path -a $HOME/bin
 # add yarn global if yarn exists
-if which -a yarn &> /dev/null
-  fish_add_path -a $(yarn global bin)
+if which -a yarn &>/dev/null
+    fish_add_path -a $(yarn global bin)
 end
 
 # manually override this in ~/.fish_local.fish if needed
-set -gx COPYCMD  'xclip -i'
+set -gx COPYCMD 'xclip -i'
 set -gx PASTECMD 'xclip -o'
 
 # don't greet me!
 set -gx fish_greeting
 
 ### set an editor
-set -gx EDITOR "vim"
-set -gx VISUAL "vim"
+set -gx EDITOR vim
+set -gx VISUAL vim
 
 set -gx STARSHIP_CONFIG "$DOTFILES_DIR/configs/starship/presets/pure.toml"
 # set -gx STARSHIP_CONFIG "$DOTFILES_DIR/configs/starship/presets/tokyo-night.toml"
