54d31d086f180118afce5882999d832178a18c4d
rykugur <rollhax@gmail.com>
2023-07-06 22:43:50 -0500

    updates to i3, pull polybar in

---
+++ configs/i3/polybar/launch.fish
@@ -0,0 +1,12 @@
+#!/usr/bin/env fish
+
+# kill any existing bars
+polybar-msg cmd quit
+
+# echo a separator
+echo --- | tee -a /tmp/polybar_primary.log /tmp/polybar_secondary.log
+
+polybar primary -c $DOTFILES_DIR/configs/i3/polybar/config.ini 2>&1 | tee -a /tmp/polybar_primary.log &
+disown
+polybar secondary -c $DOTFILES_DIR/configs/i3/polybar/config.ini 2>&1 | tee -a /tmp/polybar_secondary.log &
+disown
