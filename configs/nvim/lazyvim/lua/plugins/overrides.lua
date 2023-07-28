d79ecfc3d145778ad53282fed758fef38926e1ac
rykugur <rollhax@gmail.com>
2023-07-28 18:27:58 -0500

    update plugins and add nvim-window-picker, override l keybind for neo-tree

--- configs/nvim/lazyvim/lua/plugins/overrides.lua
+++ configs/nvim/lazyvim/lua/plugins/overrides.lua
@@ -1,13 +1,41 @@
-return {{
-    "folke/flash.nvim",
-    opts = {
-        modes = {
-            char = {
-                label = {
-                    exclude = "jkl;iardc"
-                },
-                keys = {"f", "F", "t", "T"}
-            }
-        }
-    }
-}}
+return {
+	{
+		"folke/flash.nvim",
+		opts = {
+			modes = {
+				char = {
+					label = {
+						exclude = "jkl;iardc",
+					},
+					keys = { "f", "F", "t", "T" },
+				},
+			},
+		},
+	},
+	{
+		"s1n7ax/nvim-window-picker",
+		name = "window-picker",
+		event = "VeryLazy",
+		version = "2.*",
+		config = function()
+			require("window-picker").setup({
+				hint = "floating-big-letter",
+			})
+		end,
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		config = function()
+			require("neo-tree").setup({
+				filesystem = {
+					window = {
+						mappings = {
+							-- revert 'l' key to movement
+							["l"] = "noop",
+						},
+					},
+				},
+			})
+		end,
+	},
+}
