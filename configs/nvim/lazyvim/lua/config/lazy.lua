3d1e979121e6e83dc5d0a1bb97d1af409b387b5c
rykugur <rollhax@gmail.com>
2023-09-28 20:06:19 -0500

    lazyvim tweaks

--- configs/nvim/lazyvim/lua/config/lazy.lua
+++ configs/nvim/lazyvim/lua/config/lazy.lua
@@ -14,7 +14,9 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.typescript" },
 		{ import = "lazyvim.plugins.extras.lang.json" },
 		{ import = "lazyvim.plugins.extras.ui.mini-animate" },
+		{ import = "lazyvim.plugins.extras.linting.eslint" },
 		-- import/override with your plugins
+		{ import = "lazyvim.plugins.extras.formatting.prettier" },
 		{ import = "plugins" },
 	},
 	defaults = {
--- configs/nvim/lazyvim/lua/plugins/alpha-nvim.lua
+++ configs/nvim/lazyvim/lua/plugins/alpha-nvim.lua
@@ -14,13 +14,12 @@ return {
     ]]
 			dashboard.section.header.val = vim.split(logo, "\n")
 			dashboard.section.buttons.val = {
-				dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-				dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
-				dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
-				dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
+				dashboard.button("d", "" .. " dotfiles", ":cd ~/gits/dotfiles/<CR> | :Neotree<CR>"),
+				-- dashboard.button("f", "" .. "  fish", ":cd ~/gits/dotfiles/configs/fish/ | :Neotree<CR>"),
 				dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
+				-- dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+				dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
 				dashboard.button("s", " " .. " Restore Session", [[:lua require("persistence").load() <cr>]]),
-				dashboard.button("q", " " .. " Quit", ":qa<CR>"),
 			}
 		end,
 	},
