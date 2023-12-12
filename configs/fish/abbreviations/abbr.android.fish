87845eb65fc31cb6bcb534cb2e594e0246a1ed02
rykugur <rollhax@gmail.com>
2023-12-12 14:36:02 -0600

    reverse

--- configs/fish/abbreviations/abbr.android.fish
+++ configs/fish/abbreviations/abbr.android.fish
@@ -1,4 +1,5 @@
-abbr --add --global adb.reverse adb reverse tcp:8081 tcp:8081
+abbr --add --global adb.reverse 'adb reverse tcp:8081 tcp:8081; adb reverse tcp:8080 tcp:8080'
 abbr --add --global adb.start adb shell am start
 abbr --add --global adb.reset-perms adb shell pm reset-permissions
 abbr --add --global .gw ./gradlew
+
