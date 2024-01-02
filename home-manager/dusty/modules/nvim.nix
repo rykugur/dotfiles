188cf3e52903cc6c1862ceb868c3edd79737ff71
rykugur <rollhax@gmail.com>
2024-01-02 09:58:46 -0600

    nvim test

--- home-manager/dusty/modules/nvim.nix
+++ home-manager/dusty/modules/nvim.nix
@@ -7,9 +7,18 @@
     enable = true;
   };
 
-  home.file.".config/nvim" = {
-    source = ../../../configs/nvim/lazyvim;
-    recursive = true;
-    force = true;
+  home.file = {
+    ".config/nvim/init.lua" = {
+      source = ../../../configs/nvim/lazyvim/init.lua;
+    };
+    ".config/nvim/lua" = {
+      source = ../../../configs/nvim/lazyvim/lua;
+      recursive = true;
+    };
   };
+
+  # home.file.".config/nvim" = {
+  #   source = ../../../configs/nvim/lazyvim;
+  #   recursive = true;
+  # };
 }
