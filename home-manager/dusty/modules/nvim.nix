85cb726ca720d48d7889161f04b447b94f9dfaff
rykugur <rollhax@gmail.com>
2024-01-02 10:38:12 -0600

    fix lazy-lock.json + read-only

--- home-manager/dusty/modules/nvim.nix
+++ home-manager/dusty/modules/nvim.nix
@@ -8,12 +8,12 @@
   };
 
   home.file = {
-    ".config/nvim/init.lua" = {
-      source = ../../../configs/nvim/lazyvim/init.lua;
-    };
+    ".config/nvim/init.lua".source = ../../../configs/nvim/lazyvim/init.lua;
     ".config/nvim/lua" = {
       source = ../../../configs/nvim/lazyvim/lua;
       recursive = true;
     };
+    ".config/nvim/lazy-lock.json".source = config.lib.file.mkOutOfStoreSymlink
+      "${config.home.homeDirectory}/.dotfiles/configs/nvim/lazyvim/lazy-lock.json";
   };
 }
