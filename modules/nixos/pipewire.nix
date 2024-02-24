d107889fed0b55517196d068dca625abe1af50d7
rykugur <rollhax@gmail.com>
2024-02-24 11:35:32 -0600

    always enalbe module-echo-cancel

--- modules/nixos/pipewire.nix
+++ modules/nixos/pipewire.nix
@@ -7,11 +7,24 @@
   services = {
     pipewire = {
       enable = true;
+
+      extraConfig.pipewire-pulse = {
+        "95-load-echo-cancel" = {
+          "pulse.cmd" = [
+            {
+              cmd = "load-module";
+              args = "module-echo-cancel";
+            }
+          ];
+        };
+      };
+
       # alsa is not required but helpful for 32 bit apps, particularly older games
       alsa = {
         enable = true;
         support32Bit = true;
       };
+
       pulse.enable = true;
 
       # lowLatency = {
