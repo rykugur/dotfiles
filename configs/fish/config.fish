3b222c5a1bafdd6a53623a5472439df26037538c
rykugur <rollhax@gmail.com>
2024-03-31 12:51:28 -0500

    set SSH_AUTH_SOCK for interactive sessions only

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -1,5 +1,5 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    set -gx SSH_AUTH_SOCK ~/.1password/agent.sock
 end
 
 set -q __fish_personal_dotfiles_sourced; and exit
