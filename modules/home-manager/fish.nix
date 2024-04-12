099427d3acb3ba87a88c6b6b0a724802d18fec63
rykugur <rollhax@gmail.com>
2024-04-11 20:04:19 -0500

    wtf

--- modules/home-manager/fish.nix
+++ modules/home-manager/fish.nix
@@ -1,14 +1,14 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     babelfish
-    
+
     fishPlugins.autopair
     fishPlugins.grc
     fishPlugins.fzf-fish
     fishPlugins.tide
     fishPlugins.z
   ];
-  
+
   programs.fish = {
     enable = true;
     interactiveShellInit = ''
