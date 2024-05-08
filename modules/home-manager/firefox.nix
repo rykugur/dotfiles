649558b30b4f7b2b6407962217d0d4bca2b3b1d1
rykugur <rollhax@gmail.com>
2024-05-07 20:54:30 -0500

    update le firefox

--- modules/home-manager/firefox.nix
+++ modules/home-manager/firefox.nix
@@ -5,6 +5,27 @@ let
     desktopName = "Plex Media Player";
     exec = "${pkgs.firefox}/bin/firefox --P plex";
   };
+  ArcWTF = pkgs.stdenv.mkDerivation rec {
+    name = "arcWTF";
+    version = "1.2-firefox";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "KiKaraage";
+      repo = "ArcWTF";
+      rev = "v${version}";
+      hash = "sha256-c1md5erWAqfmpizNz2TrM1QyUnnkbi47thDBMjHB4H0=";
+    };
+
+    dontPatch = true;
+    dontBuild = true;
+    dontConfigure = true;
+    dontFixup = true;
+
+    installPhase = ''
+      mkdir -p $out/chrome
+      cp -r $src/* $out/chrome
+    '';
+  };
 in
 {
   home.packages = [
@@ -24,24 +45,56 @@ in
         id = 0;
         settings = {
           "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+          "svg.context-properties.content.enabled" = true;
+          "uc.tweak.popup-search" = true;
         };
-        userChrome = ''
-          #TabsToolbar {
-            display: none;
-          }
-        '';
+        isDefault = true;
+        search = {
+          force = true;
+          default = "Google";
+          order = [ "Google" ];
+          engines = {
+            "Nix Packages" = {
+              urls = [{
+                template = "https://search.nixos.org/packages";
+                params = [
+                  { name = "type"; value = "packages"; }
+                  { name = "query"; value = "{searchTerms}"; }
+                ];
+              }];
+              icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = [ "@np" ];
+            };
+            "NixOS Wiki" = {
+              urls = [{ template = "https://nixos.wiki/index.php?search={searchTerms}"; }];
+              iconUpdateURL = "https://nixos.wiki/favicon.png";
+              updateInterval = 24 * 60 * 60 * 1000; # every day
+              definedAliases = [ "@nw" ];
+            };
+            "Home-manager Options" = {
+              urls = [{ template = "https://home-manager-options.extranix.com/?query={searchTerms}&release=master"; }];
+              iconUpdateURL = "https://home-manager-options.extranix.com/favicon.png";
+              updateInterval = 24 * 60 * 60 * 1000; # every day
+              definedAliases = [ "@hm" ];
+            };
+            "Bing".metaData.hidden = true;
+            "Google".metaData.alias = "@g"; # builtin engines only support specifying one additional alias
+          };
+        };
       };
       plex = {
         id = 1;
         settings = {
           "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
         };
-        userChrome = ''
-          #TabsToolbar {
-            display: none;
-          }
-        '';
       };
+    };
+  };
+
+  home.file = {
+    ".mozilla/firefox/default/chrome" = {
+      source = "${ArcWTF}/chrome";
+      recursive = true;
     };
   };
 
