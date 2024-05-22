aa9a59cf38dad0be5abe6e85915bf4320ad43721
rykugur <rollhax@gmail.com>
2024-05-21 19:58:19 -0500

    a couple tweaks

--- hosts/taln/configuration.nix
+++ hosts/taln/configuration.nix
@@ -16,6 +16,7 @@
       outputs.nixosModules.pipewire
       inputs.nix-gaming.nixosModules.pipewireLowLatency
 
+      outputs.nixosModules.gnome
       outputs.nixosModules.hyprland
       outputs.nixosModules.ssh
 
@@ -29,8 +30,6 @@
     ]);
 
   hardware = {
-    cpu.intel.updateMicrocode = true;
-
     nvidia = {
       modesetting.enable = true; #required
 
@@ -169,8 +168,16 @@
       # Deduplicate and optimize nix store
       auto-optimise-store = true;
 
-      substituters = [ "https://hyprland.cachix.org" ];
-      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+      substituters = [
+        "https://hyprland.cachix.org"
+        "https://nix-gaming.cachix.org"
+        "https://nix-citizen.cachix.org"
+      ];
+      trusted-public-keys = [
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+        "nix-citizen.cachix.org-1:lPMkWc2X8XD4/7YPEEwXKKBg+SVbYTVrAaLA2wQTKCo="
+      ];
     };
   };
 
@@ -186,7 +193,6 @@
     home-manager
     neovim
   ] ++ [
-    (import ../../scripts/nvidia-offload.nix { inherit pkgs; })
     (import ../../scripts/hyprland-suspend.nix { inherit pkgs; })
   ];
 
