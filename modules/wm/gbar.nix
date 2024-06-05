b5f9a8f5c8ead2618702f7a2eeb59ff78604ce1f
rykugur <rollhax@gmail.com>
2024-06-05 11:51:45 -0500

    use pkgs.system

--- modules/wm/gbar.nix
+++ modules/wm/gbar.nix
@@ -2,18 +2,18 @@
   config,
   lib,
   inputs,
+  pkgs,
   username,
   ...
 }: let
   cfg = config.programs.gbarz;
-  system = config.system.build.system;
 in {
   options.programs.gbarz.enable = lib.mkEnableOption "Enable gbar.";
 
   config = lib.mkIf cfg.enable {
     home-manager.users.${username} = {
       imports = [
-        inputs.gBar.homeManagerModules.${system}.default
+        inputs.gBar.homeManagerModules.${pkgs.system}.default
       ];
 
       programs.gBar = {
