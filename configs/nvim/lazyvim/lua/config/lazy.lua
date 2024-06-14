f38d35b5a73168780c292e147964102ea85c5d27
rykugur <rollhax@gmail.com>
2024-06-14 11:11:36 -0500

    switch navic extra

--- configs/nvim/lazyvim/lua/config/lazy.lua
+++ configs/nvim/lazyvim/lua/config/lazy.lua
@@ -17,7 +17,6 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.formatting.prettier" },
 		{ import = "lazyvim.plugins.extras.lang.json" },
 		{ import = "lazyvim.plugins.extras.ui.mini-animate" },
-		{ import = "lazyvim.plugins.extras.editor.navic" },
 		{ import = "lazyvim.plugins.extras.test.core" },
 		{ import = "lazyvim.plugins.extras.util.dot" },
 		-- import/override with your plugins
