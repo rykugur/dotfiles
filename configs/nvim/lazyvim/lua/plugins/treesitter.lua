bc2fa99b4d3d20a87b377ec93a8272089f6dc6d2
rykugur <rollhax@gmail.com>
2024-06-14 11:02:41 -0500

    enabled nix lazyextra

--- configs/nvim/lazyvim/lua/plugins/treesitter.lua
+++ configs/nvim/lazyvim/lua/plugins/treesitter.lua
@@ -1,17 +1,14 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		opts = function(_, opts)
-			if type(opts.ensure_installed) == "table" then
-				vim.list_extend(opts.ensure_installed, {
-					"fish",
-					"norg",
-					"http",
-					"json",
-					"nix",
-					"lua",
-				})
-			end
-		end,
+		opts = {
+			ensure_installed = {
+				"fish",
+				"norg",
+				"http",
+				"json",
+				"lua",
+			},
+		},
 	},
 }
