2c9f6a4055f19bd81b07a6bdb6df6683f114be4a
rykugur <rollhax@gmail.com>
2024-06-17 14:23:19 -0500

    remove un-necessary loader

--- configs/nvim/lazyvim/lua/plugins/snippets.lua
+++ configs/nvim/lazyvim/lua/plugins/snippets.lua
@@ -2,9 +2,8 @@ return {
 	{
 		"L3MON4D3/LuaSnip",
 		init = function()
-			require("luasnip.loaders.from_vscode").lazy_load()
 			require("luasnip.loaders.from_vscode").lazy_load({
-				paths = { "~/.dotfiles/configs/snippets" },
+				paths = { "~/.dotfiles/configs/snippets", "~/.local/snippets" },
 			})
 		end,
 	},
