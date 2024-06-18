be8a9ca634ac4720e19991d8266b83a232ca3ec5
rykugur <rollhax@gmail.com>
2024-06-17 23:23:18 -0500

    adds nd wrapper func

--- configs/fish/ez/nixos.fish
+++ configs/fish/ez/nixos.fish
@@ -1,8 +1,25 @@
 abbr --add --global nb "nix build"
 
-abbr --add --global nd "nix develop ~/.dotfiles#default --command fish"
-abbr --add --global ndr "nix develop ~/.dotfiles#react --command fish"
-alias ndvim "nix develop ~/.dotfiles#nvim"
+alias ndl "nd --lua"
+alias ndvim "nd --nvim"
+alias ndr "nd --react"
+
+function nd --description "wrapper for my personal nix develop shell targets"
+    argparse --name=nd l/lua n/nvim r/react -- $argv
+    or return
+
+    set -l _target default
+
+    test -n "$_flag_l"; or test -n "$_flag_lua"; and set _target lua
+    test -n "$_flag_n"; or test -n "$_flag_nvim"; and set _target nvim
+    test -n "$_flag_r"; or test -n "$_flag_react"; and set _target react
+
+    nix develop $HOME/.dotfiles\#$_target --command fish
+end
+
+complete -c nd -s lua -l lua -a lua
+complete -c nd -s r -l react -a react
+complete -c nd -s n -l nvim -a nvim
 
 abbr --add --global nr "nix repl"
 abbr --add --global nrn "nix repl --file '<nixpkgs>'"
