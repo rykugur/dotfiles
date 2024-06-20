c28456d7f954b68e9dd7df28b894acaef2b96c9d
rykugur <rollhax@gmail.com>
2024-06-19 20:27:50 -0500

    add dotfiles

--- configs/nvim/lazyvim/lua/plugins/lazydev.lua
+++ configs/nvim/lazyvim/lua/plugins/lazydev.lua
@@ -9,6 +9,7 @@ return {
 					{ path = "luvit-meta/library", words = { "vim%.uv" } },
 					{ path = "LazyVim", words = { "LazyVim" } },
 					{ path = "lazy.nvim", words = { "LazyVim" } },
+					"~/.dotfiles",
 				},
 			},
 		},
