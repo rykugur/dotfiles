d7d0f70ac7dda6bb2ca52fb38d7b409f22217177
rykugur <rollhax@gmail.com>
2024-07-03 16:30:52 -0500

    remove navic again, switch from luasnip to native snippets

--- configs/nvim/lazyvim/lua/plugins/snippets.lua
+++ configs/nvim/lazyvim/lua/plugins/snippets.lua
@@ -1,16 +1,8 @@
 return {
-  {
-    "L3MON4D3/LuaSnip",
-    init = function()
-      require("luasnip.loaders.from_vscode").lazy_load({
-        paths = { "~/.dotfiles/configs/snippets", "~/.local/snippets" },
-      })
-    end,
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    opts = function(_, opts)
-      table.insert(opts.sources, { name = "luasnip" })
-    end,
+  "garymjr/nvim-snippets",
+  opts = {
+    extended_filetypes = {
+      typescript = { "javascript" },
+    },
   },
 }
