e8a586803acfdaec5546d51bde741e041951d6fc
rykugur <rollhax@gmail.com>
2024-07-08 13:57:01 -0500

    decided I like this

--- configs/nvim/lazyvim/lua/plugins/interface.lua
+++ configs/nvim/lazyvim/lua/plugins/interface.lua
@@ -1,5 +1,16 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        styles = {
+          comments = { "italic" },
+        },
+      })
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
