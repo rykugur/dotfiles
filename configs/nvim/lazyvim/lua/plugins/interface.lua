5e0c855e2ae7607b74b6a56a72ff3eaf7e56f162
rykugur <rollhax@gmail.com>
2024-07-10 13:59:00 -0500

    ignore svg

--- configs/nvim/lazyvim/lua/plugins/interface.lua
+++ configs/nvim/lazyvim/lua/plugins/interface.lua
@@ -86,6 +86,9 @@ return {
     "nvim-telescope/telescope.nvim",
     opts = {
       defaults = {
+        file_ignore_patterns = {
+          "svg",
+        },
         layout_strategy = "vertical",
       },
     },
