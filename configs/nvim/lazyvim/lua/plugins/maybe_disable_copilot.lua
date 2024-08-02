9d99c241f1cbc78dcad43a471c43eb7a988823c8
rykugur <rollhax@gmail.com>
2024-08-01 19:31:06 -0500

    disable copilot if env var

---
+++ configs/nvim/lazyvim/lua/plugins/maybe_disable_copilot.lua
@@ -0,0 +1,9 @@
+local disabled = os.getenv("DISABLE_COPILOT") == "true"
+
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    enabled = not disabled,
+  },
+  { "zbirenbaum/copilot.lua", enabled = not disabled },
+}
