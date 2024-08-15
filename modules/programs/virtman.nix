cb31e548f90f830463f008e76eedc27ec04466b9
rykugur <rollhax@gmail.com>
2024-08-14 21:01:06 -0500

    virtman tweaks

--- modules/programs/virtman.nix
+++ modules/programs/virtman.nix
@@ -1,11 +1,29 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, username, ... }:
 let cfg = config.modules.programs.virtman;
 in {
   options.modules.programs.virtman.enable =
     lib.mkEnableOption "Enable Virt-manager";
 
   config = lib.mkIf cfg.enable {
-    virtualisation.libvirtd.enable = true;
+    virtualisation.libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        runAsRoot = true;
+        swtpm.enable = true;
+        ovmf = {
+          enable = true;
+          packages = [
+            (pkgs.OVMF.override {
+              secureBoot = true;
+              tpmSupport = true;
+            }).fd
+          ];
+        };
+      };
+    };
     programs.virt-manager.enable = true;
+
+    users.users.${username}.extraGroups = [ "libvirtd" ];
   };
 }
