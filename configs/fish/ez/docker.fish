7dc64bcd0b07bb58cd4bffc215fd392e0017dcb0
rykugur <rollhax@gmail.com>
2024-09-14 00:04:38 -0500

    grmbl

--- configs/fish/ez/docker.fish
+++ configs/fish/ez/docker.fish
@@ -1,12 +1,13 @@
 ### docker
-abbr --add --global dc docker-compose
-abbr --add --global dcb docker-compose build
-abbr --add --global dcl docker-compose logs
-abbr --add --global dclf docker-compose logs -f
-abbr --add --global dcr docker-compose restart
-abbr --add --global dcu docker-compose up
-abbr --add --global dcud docker-compose up -d
-abbr --add --global dcd docker-compose down
+set -l DOCKER_COMPOSE_COMMAND "docker compose"
+abbr --add --global dc $DOCKER_COMPOSE_COMMAND
+abbr --add --global dcb $DOCKER_COMPOSE_COMMAND build
+abbr --add --global dcl $DOCKER_COMPOSE_COMMAND logs
+abbr --add --global dclf $DOCKER_COMPOSE_COMMAND logs -f
+abbr --add --global dcr $DOCKER_COMPOSE_COMMAND restart
+abbr --add --global dcu $DOCKER_COMPOSE_COMMAND up
+abbr --add --global dcud $DOCKER_COMPOSE_COMMAND up -d
+abbr --add --global dcd $DOCKER_COMPOSE_COMMAND down
 abbr --add --global drit 'docker run -it'
 abbr --add --global docker.clean 'docker rmi (docker images -f dangling=true -q)'
 abbr --add --global lzd lazydocker
