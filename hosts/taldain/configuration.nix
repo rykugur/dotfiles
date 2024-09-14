e8ad0588c448b230a62472c0a6baf5ce6ac6d00d
rykugur <rollhax@gmail.com>
2024-09-13 23:43:22 -0500

    blocky

--- hosts/taldain/configuration.nix
+++ hosts/taldain/configuration.nix
@@ -96,6 +96,38 @@
     services = { ssh.enable = true; };
   };
 
+  services.blocky = {
+    enable = true;
+    settings = {
+      ports.dns = 53; # Port for incoming DNS Queries.
+      upstreams.groups.default = [
+        "https://one.one.one.one/dns-query" # Using Cloudflare's DNS over HTTPS server for resolving queries.
+      ];
+      # For initially solving DoH/DoT Requests when no system Resolver is available.
+      bootstrapDns = {
+        upstream = "https://one.one.one.one/dns-query";
+        ips = [ "1.1.1.1" "1.0.0.1" ];
+      };
+      #Enable Blocking of certian domains.
+      blocking = {
+        blackLists = {
+          #Adblocking
+          ads = [
+            "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
+          ];
+          #Another filter for blocking adult sites
+          adult = [ "https://blocklistproject.github.io/Lists/porn.txt" ];
+          #You can add additional categories
+        };
+        #Configure what block categories are used
+        clientGroupsBlock = {
+          default = [ "ads" ];
+          kids = [ "ads" "adult" ];
+        };
+      };
+    };
+  };
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.11";
 }
