178d8153f4895774d6dc92849f1ce0f396135b2f
rykugur <rollhax@gmail.com>
2024-09-14 00:14:18 -0500

    acme

--- hosts/taldain/configuration.nix
+++ hosts/taldain/configuration.nix
@@ -157,6 +157,10 @@
       };
     };
   };
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "rollhax@gmail.com";
+  };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.11";
