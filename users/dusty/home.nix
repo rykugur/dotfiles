c5161ab56e8c5ec7376882ce817cc8fc9f73fa4d
Dusty <rollhax@gmail.com>
2025-02-09 20:53:43 -0600

    home.nix age key change

--- users/dusty/home.nix
+++ users/dusty/home.nix
@@ -68,11 +68,9 @@ in {
 
   sops = {
     defaultSopsFile = ../../hosts/${hostname}/secrets.yaml;
-    # age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
 
     age = {
-      sshKeyPaths = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
-      generateKey = true;
+      keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
     };
 
     secrets = {
