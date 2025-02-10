c920cad42e843a2ee7072d7ee9c1291acbf5df01
rykugur <rollhax@gmail.com>
2025-02-10 11:01:52 -0600

    comment out nupm; tried carapace, commented for now

--- modules/home-manager/nushell.nix
+++ modules/home-manager/nushell.nix
@@ -7,12 +7,12 @@ let
     rev = "e380c8a355b4340c26dc51c6be7bed78f87b0c71";
     sha256 = "sha256-b2AeWiHRz1LbiGR1gOJHBV3H56QP7h8oSTzg+X4Shk8=";
   };
-  nupm = pkgs.fetchFromGitHub {
-    owner = "nushell";
-    repo = "nupm";
-    rev = "7e3e5779ff86a1b8dadcf7a90eee2e3dcfe449df";
-    sha256 = "sha256-BNFBQ9kK2/P7mjdBqMj/8cbBPVogK0n1qcx6dx9mer8=";
-  };
+  # nupm = pkgs.fetchFromGitHub {
+  #   owner = "nushell";
+  #   repo = "nupm";
+  #   rev = "7e3e5779ff86a1b8dadcf7a90eee2e3dcfe449df";
+  #   sha256 = "sha256-BNFBQ9kK2/P7mjdBqMj/8cbBPVogK0n1qcx6dx9mer8=";
+  # };
 in {
   options.rhx.nushell = {
     enable = lib.mkEnableOption "Enable nushell home-manager module.";
@@ -54,6 +54,13 @@ in {
       enableNushellIntegration = true;
       nix-direnv.enable = true;
     };
+
+    # programs.carapace = {
+    #   enable = true;
+    #   enableBashIntegration = false;
+    #   enableNushellIntegration = true;
+    #   enableZshIntegration = false;
+    # };
 
     programs.starship = { enableNushellIntegration = true; };
 
