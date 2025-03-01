a283acad81e15c461d51296942cd5f39736fcbf4
rykugur <rollhax@gmail.com>
2025-03-01 15:55:42 -0600

    sc tweaks

--- modules/nixos/starcitizen.nix
+++ modules/nixos/starcitizen.nix
@@ -7,6 +7,12 @@ in {
     lib.mkEnableOption "Enable starcitizen nixOS module";
 
   config = lib.mkIf cfg.enable {
+    nix.settings = {
+      substituters = [ "https://nix-citizen.cachix.org" ];
+      trusted-public-keys = [
+        "nix-citizen.cachix.org-1:lPMkWc2X8XD4/7YPEEwXKKBg+SVbYTVrAaLA2wQTKCo="
+      ];
+    };
 
     nix-citizen.starCitizen = {
       enable = true;
@@ -17,17 +23,7 @@ in {
       '';
       # Experimental script
       helperScript.enable = true;
-      umu.enable = true;
-    };
-
-    nix.settings = {
-      # TODO: move these to respective module files
-      substituters =
-        [ "https://nix-gaming.cachix.org" "https://nix-citizen.cachix.org" ];
-      trusted-public-keys = [
-        "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-        "nix-citizen.cachix.org-1:lPMkWc2X8XD4/7YPEEwXKKBg+SVbYTVrAaLA2wQTKCo="
-      ];
+      # umu.enable = true;
     };
   };
 }
