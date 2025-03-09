e7d42c2d9683f61f573ebb7c22727eecc71ff825
rykugur <rollhax@gmail.com>
2025-03-09 01:09:40 -0600

    add wheel to trusted-users

--- hosts/jezrien/configuration.nix
+++ hosts/jezrien/configuration.nix
@@ -20,6 +20,8 @@
     extra-substituters = [ "https://helix.cachix.org" ];
     extra-trusted-public-keys =
       [ "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs=" ];
+
+    trusted-users = [ "root" "@wheel" ];
   };
 
   boot = {
