4b8f99cafe46b34d6b494aaedaf56c228c7d09f4
rykugur <rollhax@gmail.com>
2025-03-23 23:13:14 -0500

    getting 1password/polkit working

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -66,17 +66,21 @@ in {
       gtk.enable = true;
     };
 
-    services.hyprpaper = {
-      enable = true;
-      settings = {
-        ipc = "on";
-        splash = false;
-        splash_offset = 2.0;
+    services = {
+      hyprpaper = {
+        enable = true;
+        settings = {
+          ipc = "on";
+          splash = false;
+          splash_offset = 2.0;
 
-        preload = [ "~/.wallpapers/cyberpunk_skull.png" ];
+          preload = [ "~/.wallpapers/cyberpunk_skull.png" ];
 
-        wallpaper = [ ",~/.wallpapers/cyberpunk_skull.png" ];
+          wallpaper = [ ",~/.wallpapers/cyberpunk_skull.png" ];
+        };
       };
+
+      hyprpolkitagent.enable = true;
     };
 
     xdg = {
