2276cba4690b0ed6135efaefb2c34adf7919547b
rykugur <rollhax@gmail.com>
2025-03-31 12:36:47 -0500

    cleanup

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,5 +1,4 @@
-# This file defines overlays
-{ inputs, ... }: {
+{ ... }: {
   # This one brings our custom packages from the 'pkgs' directory
   additions = final: _prev: import ../pkgs { pkgs = final; };
 
@@ -7,11 +6,6 @@
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
   modifications = final: prev: {
-    # patool = prev.patool.overrideAttrs (old: {
-    #   doCheck = false;
-    #   doInstallCheck = false;
-    #   pytestCheckPhase = false;
-    # });
     vscode-langservers-extracted =
       prev.vscode-langservers-extracted.overrideAttrs (oldAttrs: rec {
         version = "4.8.0";
@@ -53,13 +47,4 @@
     #   ];
     # });
   };
-
-  # When applied, the unstable nixpkgs set (declared in the flake inputs) will
-  # be accessible through 'pkgs.unstable'
-  # unstable-packages = final: _prev: {
-  #   unstable = import inputs.nixpkgs-unstable {
-  #     system = final.system;
-  #     config.allowUnfree = true;
-  #   };
-  # };
 }
