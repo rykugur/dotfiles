7ba0bef96178fee07e2bdc7751073a660698ed21
rykugur <rollhax@gmail.com>
2025-04-19 16:10:03 -0500

    disable vi mode for now? update flake

--- configs/nu/config.nu
+++ configs/nu/config.nu
@@ -7,7 +7,7 @@ use ./abbreviations.nu
 $env.config.keybindings = $env.config.keybindings | append (abbreviations get_keybinds)
 $env.config.menus = $env.config.menus | append (abbreviations get_menus)
 
-$env.config.edit_mode = 'vi'
+# $env.config.edit_mode = 'vi'
 $env.config.keybindings ++= [
   {
     name: hint_completion
--- pkgs/gameglass.nix
+++ pkgs/gameglass.nix
@@ -6,6 +6,6 @@ appimageTools.wrapType2 rec {
   src = fetchurl {
     url = "https://download.gameglass.gg/hub/GameGlass.AppImage";
     # TODO: this will fail if they change the AppImage file
-    sha256 = "sha256-HlZ3MUGX4W62l4sAnvjzvMLDUCVHdJQEtEUOQCz+dJo=";
+    sha256 = "sha256-V0dHp2+IeiR1yy7cYsJQV/Zx1BGYOrZXjcTvQ5jaw30=";
   };
 }
