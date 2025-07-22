3c83affec39d2cee1e682294f7d4b85cb58b9a37
rykugur <rollhax@gmail.com>
2025-07-22 12:40:18 -0500

    never use(d) these

--- shells/default.nix
+++ shells/default.nix
@@ -1,35 +1,4 @@
-{ inputs, pkgs, ... }: {
-
+{ pkgs, ... }: {
   default =
     pkgs.mkShell { packages = [ pkgs.just pkgs.sops pkgs.nixos-anywhere ]; };
-
-  lua = pkgs.mkShell {
-    packages = [
-      inputs.luarocks-nix.packages.${pkgs.system}.default
-      pkgs.love
-      pkgs.lua
-      pkgs.nurl
-    ];
-
-    shellHook = ''
-      eval $(luarocks path --bin)
-      fish
-      exit
-    '';
-  };
-
-  kubes = pkgs.mkShell {
-    buildInputs = with pkgs; [
-      k9s
-      kubectl
-      kubectx
-      kubecolor
-
-      kubernetes-helm
-      helmfile
-
-      docker
-      minikube
-    ];
-  };
 }
