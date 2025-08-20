50a059411d03f7b4c57a8902e3ecd2de6eca5d6b
rykugur <rollhax@gmail.com>
2025-08-19 21:38:04 -0500

    remove 2

--- modules/home-manager/hyprland/hyprlock.nix
+++ modules/home-manager/hyprland/hyprlock.nix
@@ -1,11 +1,4 @@
 { config, lib, pkgs, ... }: {
-  # let
-  #   catppuccin-hyprlock = pkgs.fetchgit {
-  #     url = "https://github.com/catppuccin/hyprlock";
-  #     rev = "958e70b1cd8799defd16dee070d07f977d4fd76b";
-  #     sha256 = "sha256-l4CbAUeb/Tg603QnZ/VWxuGqRBztpHN0HLX/h8ndc5w=";
-  #   };
-  # in {
   config = lib.mkIf config.rhx.hyprland.enable {
     programs.hyprlock = {
       enable = true;
