08693e49af05e38ebd317f2e019077bab3689f4b
rykugur <rollhax@gmail.com>
2025-08-23 17:09:19 -0500

    add waybar settings

--- modules/home-manager/hyprland/waybar.nix
+++ modules/home-manager/hyprland/waybar.nix
@@ -1,3 +1,236 @@
 { config, lib, pkgs, ... }:
 let cfg = config.rhx.hyprland.waybar;
-in { config = lib.mkIf cfg.enable { }; }
+in {
+  config = lib.mkIf cfg.enable {
+    programs.waybar = {
+      enable = true;
+
+      settings = let ghosttyCmd = "ghostty --gtk-single-instance=true";
+      in {
+        layer = "top";
+        position = "top";
+        spacing = 0;
+        height = 26;
+        modules-left = [ "hyprland/workspaces" ];
+        modules-center = [ "clock" ];
+        modules-right = [
+          # "custom/dropbox"
+          "tray"
+          "bluetooth"
+          "network"
+          "wireplumber"
+          "cpu"
+          "power-profiles-daemon"
+          "battery"
+        ];
+        "hyprland/workspaces" = {
+          on-click = "activate";
+          format = "{icon}";
+          format-icons = {
+            default = "";
+            "1" = "1";
+            "2" = "2";
+            "3" = "3";
+            "4" = "4";
+            "5" = "5";
+            active = "󱓻";
+          };
+          persistent-workspaces = {
+            "1" = [ ];
+            "2" = [ ];
+            "3" = [ ];
+            "4" = [ ];
+            "5" = [ ];
+          };
+        };
+        cpu = {
+          interval = 5;
+          format = "󰍛";
+          on-click = "${ghosttyCmd} -e btop";
+        };
+        clock = {
+          format = "{:%A %I:%M %p}";
+          format-alt = "{:%d %B W%V %Y}";
+          tooltip = false;
+        };
+        network = {
+          format-icons = [ "󰤯" "󰤟" "󰤢" "󰤥" "󰤨" ];
+          format = "{icon}";
+          format-wifi = "{icon}";
+          format-ethernet = "󰀂";
+          format-disconnected = "󰖪";
+          tooltip-format-wifi = ''
+            {essid} ({frequency} GHz)
+            ⇣{bandwidthDownBytes}  ⇡{bandwidthUpBytes}'';
+          tooltip-format-ethernet =
+            "⇣{bandwidthDownBytes}  ⇡{bandwidthUpBytes}";
+          tooltip-format-disconnected = "Disconnected";
+          interval = 3;
+          nospacing = 1;
+          on-click = "${ghosttyCmd} -e nmcli";
+        };
+        battery = {
+          interval = 5;
+          format = "{capacity}% {icon}";
+          format-discharging = "{icon}";
+          format-charging = "{icon}";
+          format-plugged = "";
+          format-icons = {
+            charging = [ "󰢜" "󰂆" "󰂇" "󰂈" "󰢝" "󰂉" "󰢞" "󰂊" "󰂋" "󰂅" ];
+            default = [ "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹" ];
+          };
+          format-full = "Charged ";
+          tooltip-format-discharging = "{power:>1.0f}W↓ {capacity}%";
+          tooltip-format-charging = "{power:>1.0f}W↑ {capacity}%";
+          states = {
+            warning = 20;
+            critical = 10;
+          };
+        };
+        bluetooth = {
+          format = "󰂯";
+          format-disabled = "󰂲";
+          format-connected = "";
+          tooltip-format = "Devices connected: {num_connections}";
+          on-click = "blueberry";
+        };
+        wireplumber = {
+          # Changed from "pulseaudio"
+          "format" = "";
+          format-muted = "󰝟";
+          scroll-step = 5;
+          on-click = "pavucontrol";
+          tooltip-format = "Playing at {volume}%";
+          on-click-right =
+            "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"; # Updated command
+          # max-volume = 150; # Optional: allow volume over 100%
+        };
+        tray = { spacing = 13; };
+        power-profiles-daemon = {
+          format = "{icon}";
+          tooltip-format = "Power profile: {profile}";
+          tooltip = true;
+          format-icons = {
+            power-saver = "󰡳";
+            balanced = "󰊚";
+            performance = "󰡴";
+          };
+        };
+        # "custom/dropbox" = {
+        #   format = "";
+        #   on-click = "nautilus ~/Dropbox";
+        #   exec = "dropbox-cli status";
+        #   return-type = "text";
+        #   interval = 5;
+        #   tooltip = true;
+        #   tooltip-format = "{}";
+        # };
+      };
+
+      style = ''
+        @import "${pkgs.catppuccin-ports.waybar}/themes/mocha.css";
+
+        * {
+          font-family: CaskaydiaCove Nerd Font Mono;
+          font-size: 17px;
+          min-height: 0;
+        }
+
+        #waybar {
+          background: transparent;
+          color: @text;
+          margin: 5px 5px;
+        }
+
+        #workspaces {
+          border-radius: 1rem;
+          margin: 5px;
+          background-color: @surface0;
+          margin-left: 1rem;
+        }
+
+        #workspaces button {
+          color: @lavender;
+          border-radius: 1rem;
+          padding: 0.4rem;
+        }
+
+        #workspaces button.active {
+          color: @sky;
+          border-radius: 1rem;
+        }
+
+        #workspaces button:hover {
+          color: @sapphire;
+          border-radius: 1rem;
+        }
+
+        #custom-music,
+        #tray,
+        #backlight,
+        #clock,
+        #battery,
+        #pulseaudio,
+        #custom-lock,
+        #custom-power {
+          background-color: @surface0;
+          padding: 0.5rem 1rem;
+          margin: 5px 0;
+        }
+
+        #clock {
+          color: @blue;
+          border-radius: 0px 1rem 1rem 0px;
+          margin-right: 1rem;
+        }
+
+        #battery {
+          color: @green;
+        }
+
+        #battery.charging {
+          color: @green;
+        }
+
+        #battery.warning:not(.charging) {
+          color: @red;
+        }
+
+        #backlight {
+          color: @yellow;
+        }
+
+        #backlight, #battery {
+            border-radius: 0;
+        }
+
+        #pulseaudio {
+          color: @maroon;
+          border-radius: 1rem 0px 0px 1rem;
+          margin-left: 1rem;
+        }
+
+        #custom-music {
+          color: @mauve;
+          border-radius: 1rem;
+        }
+
+        #custom-lock {
+            border-radius: 1rem 0px 0px 1rem;
+            color: @lavender;
+        }
+
+        #custom-power {
+            margin-right: 1rem;
+            border-radius: 0px 1rem 1rem 0px;
+            color: @red;
+        }
+
+        #tray {
+          margin-right: 1rem;
+          border-radius: 1rem;
+        }
+      '';
+    };
+  };
+}
