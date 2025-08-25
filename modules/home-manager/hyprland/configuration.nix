70865f724a163424ebda19fa38c6bb55bac5e096
rykugur <rollhax@gmail.com>
2025-08-25 01:26:12 -0500

    smaller gaps

--- modules/home-manager/hyprland/configuration.nix
+++ modules/home-manager/hyprland/configuration.nix
@@ -20,7 +20,7 @@ in {
 
         general = {
           gaps_in = 5;
-          gaps_out = 20;
+          gaps_out = 10;
           border_size = 2;
           "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
           "col.inactive_border" = "rgba(595959aa)";
