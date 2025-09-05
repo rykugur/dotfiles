633123fb484d5e6c643ede408a078f5734c67943
rykugur <rollhax@gmail.com>
2025-09-04 22:25:26 -0500

    no really fix it

--- modules/home-manager/ssh.nix
+++ modules/home-manager/ssh.nix
@@ -24,11 +24,20 @@ in {
         enable = true;
         enableDefaultConfig = false;
 
-        extraConfig = ''
-          AddKeysToAgent yes
-        '';
-
         matchBlocks = {
+          "*" = {
+            forwardAgent = false;
+            addKeysToAgent = "no";
+            compression = false;
+            serverAliveInterval = 0;
+            serverAliveCountMax = 3;
+            hashKnownHosts = false;
+            userKnownHostsFile = "~/.ssh/known_hosts";
+            controlMaster = "no";
+            controlPath = "~/.ssh/master-%r@%n:%p";
+            controlPersist = "no";
+          };
+
           "*.local.ryk.sh" = {
             identityFile = "~/.ssh/id_ed25519";
             identitiesOnly = true;
