eb2768a5938af737b6343b0f7a1edf4c916c046f
rykugur <rollhax@gmail.com>
2025-11-20 18:06:47 -0600

    formatting

--- modules/gaming/audiorelay/default.nix
+++ modules/gaming/audiorelay/default.nix
@@ -6,8 +6,10 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    networking.firewall.allowedTCPPorts = [ 59100 ];
-    networking.firewall.allowedUDPPorts = [ 59100 ];
+    networking.firewall = {
+      allowedTCPPorts = [ 59100 ];
+      allowedUDPPorts = [ 59100 ];
+    };
 
     home-manager.users.${username}.imports = [ ./home.nix ];
   };
