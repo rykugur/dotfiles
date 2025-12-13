ee925c5389466a2a8c5783ea1c8cc6b8a5367278
rykugur <rollhax@gmail.com>
2025-12-13 01:24:13 -0600

    hi

--- hosts/nixy/home.nix
+++ hosts/nixy/home.nix
@@ -2,4 +2,10 @@
   imports = [ ../../modules/home-manager/helix.nix ];
 
   rhx.helix.enable = true;
+
+  ### leave me alone
+  programs.home-manager.enable = true;
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+  home.stateVersion = "25.05";
 }
