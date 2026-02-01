d1a84eb8b5c9947aae0512c6f6ea10906f4ac544
rykugur <rollhax@gmail.com>
2026-02-01 17:41:54 -0600

    fix nixConfig

--- flake.nix
+++ flake.nix
@@ -150,20 +150,20 @@
           };
         };
       };
-
-      nixConfig = {
-        extra-substituters = [
-          "https://hyprland.cachix.org"
-          "https://nix-gaming.cachix.org"
-          "https://nix-citizen.cachix.org"
-          "https://helix.cachix.org"
-        ];
-        extra-trusted-public-keys = [
-          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-          "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-          "nix-citizen.cachix.org-1:lPMkWc2X8XD4/7YPEEwXKKBg+SVbYTVrAaLA2wQTKCo="
-          "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
-        ];
-      };
     };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://hyprland.cachix.org"
+      "https://nix-gaming.cachix.org"
+      "https://nix-citizen.cachix.org"
+      "https://helix.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      "nix-citizen.cachix.org-1:lPMkWc2X8XD4/7YPEEwXKKBg+SVbYTVrAaLA2wQTKCo="
+      "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+    ];
+  };
 }
