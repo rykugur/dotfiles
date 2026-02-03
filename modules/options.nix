6d4ac34e3483cf7aa913b50b52443d770db92693
rykugur <rollhax@gmail.com>
2026-02-02 22:19:28 -0600

    options?

--- modules/options.nix
+++ modules/options.nix
@@ -1,19 +1,23 @@
 { lib, ... }:
 {
-  options.ryk = {
-    features = lib.mkOption {
-      type = lib.types.submodule {
-        options = {
-          helix = lib.mkEnableOption "Enable helix editor feature";
+  flake.nixosModules.jezrien =
+    { ... }:
+    {
+      options.ryk = {
+        features = lib.mkOption {
+          type = lib.types.submodule {
+            options = {
+              helix = lib.mkEnableOption "Enable helix editor feature";
+            };
+          };
+          default = { };
+          description = "Feature toggles";
+        };
+
+        username = lib.mkOption {
+          type = lib.types.str;
+          default = "dusty";
         };
       };
-      default = { };
-      description = "Feature toggles";
     };
-
-    username = lib.mkOption {
-      type = lib.types.str;
-      default = "dusty";
-    };
-  };
 }
