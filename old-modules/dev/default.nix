fcffd2f98f667e23f8bc2eb42fc00d8883899c06
rykugur <rollhax@gmail.com>
2026-02-05 16:23:49 -0600

    a couple more

--- old-modules/dev/default.nix
+++
@@ -1,9 +0,0 @@
-{ lib, config, ... }: {
-  imports = [ ./yaak ];
-
-  options.ryk.dev.enable = lib.mkEnableOption "Enable dev modules";
-
-  config = lib.mkIf config.ryk.dev.enable {
-    ryk.dev = { yaak.enable = lib.mkDefault true; };
-  };
-}
