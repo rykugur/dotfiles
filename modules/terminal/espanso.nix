7426a916a312a376fc0f11fe101077bb232c7214
rykugur <rollhax@gmail.com>
2026-03-30 12:12:21 -0500

    fix(espanso): expose as homeManager module, not flake module

--- modules/terminal/espanso.nix
+++ modules/terminal/espanso.nix
@@ -1,20 +1,23 @@
-{ lib, config, ... }:
-
-let
-  inherit (lib) types mkEnableOption mkOption mkIf;
-in
+{ ... }:
 {
-  options.espanso = {
-    snippets = mkOption {
-      type = types.listOf types.attrs;
-      default = [ ];
-      description = "List of Espanso snippet definitions";
-    };
-  };
+  flake.modules.homeManager.espanso =
+    { config, lib, ... }:
+    let
+      inherit (lib) types mkOption mkIf;
+    in
+    {
+      options.espanso = {
+        snippets = mkOption {
+          type = types.listOf types.attrs;
+          default = [ ];
+          description = "List of Espanso snippet definitions";
+        };
+      };
 
-  config = mkIf config.services.espanso.enable {
-    services.espanso.configs.default = {
-      matches = config.espanso.snippets;
+      config = mkIf config.services.espanso.enable {
+        services.espanso.configs.default = {
+          matches = config.espanso.snippets;
+        };
+      };
     };
-  };
 }
