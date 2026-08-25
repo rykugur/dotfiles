3993eb1278bb59affbf29eae719c69442d103941
rykugur <rollhax@gmail.com>
2026-08-25 12:06:19 -0500

    add cataclysm-dda and format file

--- modules/hosts/jezrien/default.nix
+++ modules/hosts/jezrien/default.nix
@@ -9,199 +9,203 @@ let
   # TODO: this can be removed once all modules are migrated
   username = "dusty";
   hmModules = config.flake.modules.homeManager;
-  mkJezrien = extraModules: inputs.nixpkgs.lib.nixosSystem {
-    modules = [
-      # WM: niri (hyprland lives at self.modules.nixos.hyprland)
-      self.modules.nixos.niri
+  mkJezrien =
+    extraModules:
+    inputs.nixpkgs.lib.nixosSystem {
+      modules = [
+        # WM: niri (hyprland lives at self.modules.nixos.hyprland)
+        self.modules.nixos.niri
 
-      self.modules.nixos.nix-defaults
-      self.modules.nixos.vasher-cache
-      self.modules.nixos.ssh
-      self.modules.nixos.helix-root
-      ./_configuration.nix
+        self.modules.nixos.nix-defaults
+        self.modules.nixos.vasher-cache
+        self.modules.nixos.ssh
+        self.modules.nixos.helix-root
+        ./_configuration.nix
 
-      inputs.home-manager.nixosModules.home-manager
-      inputs.stylix.nixosModules.stylix
+        inputs.home-manager.nixosModules.home-manager
+        inputs.stylix.nixosModules.stylix
 
-      self.modules.nixos.meta
-      self.modules.nixos.fonts
-      self.modules.nixos.login-shell
-      self.modules.nixos.stylix
+        self.modules.nixos.meta
+        self.modules.nixos.fonts
+        self.modules.nixos.login-shell
+        self.modules.nixos.stylix
 
-      self.modules.nixos.pipewire
-      # disabled: nix-gaming dxvk strictDeps/structuredAttrs regression breaks
-      # rsi-launcher eval (cross-spliced through wineprefix-preparer).
-      # starcitizen-lite below provides the LUG prereqs without the broken
-      # launcher path; re-enable this once upstream nix-gaming is fixed.
-      # self.modules.nixos.starcitizen
-      self.modules.nixos.starcitizen-lite
+        self.modules.nixos.pipewire
+        # disabled: nix-gaming dxvk strictDeps/structuredAttrs regression breaks
+        # rsi-launcher eval (cross-spliced through wineprefix-preparer).
+        # starcitizen-lite below provides the LUG prereqs without the broken
+        # launcher path; re-enable this once upstream nix-gaming is fixed.
+        # self.modules.nixos.starcitizen
+        self.modules.nixos.starcitizen-lite
 
-      self.modules.nixos._1password
-      self.modules.nixos.btrfs
-      self.modules.nixos.dusty-nfs
-      self.modules.nixos.zram
-      self.modules.nixos.zsa
+        self.modules.nixos._1password
+        self.modules.nixos.btrfs
+        self.modules.nixos.dusty-nfs
+        self.modules.nixos.zram
+        self.modules.nixos.zsa
 
-      self.modules.nixos.docker
-      self.modules.nixos.winboat
+        self.modules.nixos.docker
+        self.modules.nixos.winboat
 
-      self.modules.nixos.appimage
-      self.modules.nixos.gamemode
-      self.modules.nixos.ntsync
-      self.modules.nixos.obs-studio
-      self.modules.nixos.razer
-      self.modules.nixos.steam
-      self.modules.nixos.gaming
+        self.modules.nixos.appimage
+        self.modules.nixos.gamemode
+        self.modules.nixos.ntsync
+        self.modules.nixos.obs-studio
+        self.modules.nixos.razer
+        self.modules.nixos.steam
+        self.modules.nixos.gaming
 
-      # home-manager config
-      {
-        home-manager = {
-          useGlobalPkgs = true;
-          extraSpecialArgs = {
-            inherit
-              inputs
-              username
-              ;
-            outputs = inputs.self;
-            hostname = "jezrien";
-          };
-          backupFileExtension = "bak";
-
-          users.${username} =
-            { pkgs, ... }:
-            {
-              imports = with hmModules; [
-                # groups
-                developer
-                gaming
-                _3dp
-
-                # ai stuff
-                ai-common
-                claude-code
-                codex
-                herdr
-                hermes-agent
-                opencode
-                oh-my-pi
-
-                # individual modules
-                btop
-                espanso
-                gtk
-                eve-online
-                easyeffects
-                homelab
-                jackify
-                keebs
-                obsidian
-                # sesh
-                sops
-                starsector
-                eve-frontier
-                swappy
-                television
-                wezterm
-                zen-browser
-              ];
-
-              sops.secrets = {
-                homelab_ssh_private_key = {
-                  sopsFile = ./secrets.yaml;
-                };
-              };
-
-              home.packages = with pkgs; [
-                # nix
-                nix-prefetch-scripts
-                nixd
-                nix-index
-
-                # fonts
-                font-awesome
-
-                # desktop
-                arandr
-                cliphist
-                pinta
-                xrandr
-                xbacklight
-
-                # misc gaming
-                dolphin-emu
-
-                affine
-                baobab
-                bottom
-                fastfetch
-                fd
-                file
-                file-roller
-                kalker
-                minder
-                mousai
-                nemo
-                nitch
-                pavucontrol
-                playerctl
-                rcon-cli
-                seahorse
-                spotify
-                sshfs
-                tigervnc
-                tldr
-                vlc
-                xdg-utils
-                zenity
-
-                yt-dlg
-                yt-dlp
-
-                amdgpu_top
-                radeontop
-
-                feh
-                ristretto
-
-                libtool
-
-                telegram-desktop
-
-                google-chrome
-
-              ];
-
-              home.sessionVariables = {
-                GDK_BACKEND = "wayland";
-                PROTON_USE_NTSYNC = "1";
-                PROTON_NO_ESYNC = "1";
-                PROTON_NO_FSYNC = "1";
-              };
-
-              programs.ghostty.settings.window-decoration = "none";
-
-              ryk.defaultShell = "nushell";
-
-              xdg.enable = true;
-              programs.home-manager.enable = true;
-
-              systemd.user.startServices = "sd-switch";
-
-              home.stateVersion = "23.11";
+        # home-manager config
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            extraSpecialArgs = {
+              inherit
+                inputs
+                username
+                ;
+              outputs = inputs.self;
+              hostname = "jezrien";
             };
-        };
-      }
-    ] ++ extraModules;
-    specialArgs = {
-      inherit
-        inputs
-        # TODO: this can be removed once all modules are migrated
-        username
-        ;
-      outputs = inputs.self;
-      hostname = "jezrien";
+            backupFileExtension = "bak";
+
+            users.${username} =
+              { pkgs, ... }:
+              {
+                imports = with hmModules; [
+                  # groups
+                  developer
+                  gaming
+                  _3dp
+
+                  # ai stuff
+                  ai-common
+                  claude-code
+                  codex
+                  herdr
+                  hermes-agent
+                  opencode
+                  oh-my-pi
+
+                  # individual modules
+                  btop
+                  espanso
+                  gtk
+                  eve-online
+                  easyeffects
+                  homelab
+                  jackify
+                  keebs
+                  obsidian
+                  # sesh
+                  sops
+                  starsector
+                  eve-frontier
+                  swappy
+                  television
+                  wezterm
+                  zen-browser
+                ];
+
+                sops.secrets = {
+                  homelab_ssh_private_key = {
+                    sopsFile = ./secrets.yaml;
+                  };
+                };
+
+                home.packages = with pkgs; [
+                  # nix
+                  nix-prefetch-scripts
+                  nixd
+                  nix-index
+
+                  # fonts
+                  font-awesome
+
+                  # desktop
+                  arandr
+                  cliphist
+                  pinta
+                  xrandr
+                  xbacklight
+
+                  # misc gaming
+                  cataclysm-dda
+                  dolphin-emu
+
+                  affine
+                  baobab
+                  bottom
+                  fastfetch
+                  fd
+                  file
+                  file-roller
+                  kalker
+                  minder
+                  mousai
+                  nemo
+                  nitch
+                  pavucontrol
+                  playerctl
+                  rcon-cli
+                  seahorse
+                  spotify
+                  sshfs
+                  tigervnc
+                  tldr
+                  vlc
+                  xdg-utils
+                  zenity
+
+                  yt-dlg
+                  yt-dlp
+
+                  amdgpu_top
+                  radeontop
+
+                  feh
+                  ristretto
+
+                  libtool
+
+                  telegram-desktop
+
+                  google-chrome
+
+                ];
+
+                home.sessionVariables = {
+                  GDK_BACKEND = "wayland";
+                  PROTON_USE_NTSYNC = "1";
+                  PROTON_NO_ESYNC = "1";
+                  PROTON_NO_FSYNC = "1";
+                };
+
+                programs.ghostty.settings.window-decoration = "none";
+
+                ryk.defaultShell = "nushell";
+
+                xdg.enable = true;
+                programs.home-manager.enable = true;
+
+                systemd.user.startServices = "sd-switch";
+
+                home.stateVersion = "23.11";
+              };
+          };
+        }
+      ]
+      ++ extraModules;
+      specialArgs = {
+        inherit
+          inputs
+          # TODO: this can be removed once all modules are migrated
+          username
+          ;
+        outputs = inputs.self;
+        hostname = "jezrien";
+      };
     };
-  };
 in
 {
   flake.nixosConfigurations = {
